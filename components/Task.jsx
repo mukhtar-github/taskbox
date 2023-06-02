bbfc5cad54947ccb9816e1b4a0898f3f6dda8cb8
Mukhtar <mukhtartanimugarba@gmail.com>
2023-06-02 03:52:37 +0100

    Update changes

--- components/Task.jsx
+++ components/Task.jsx
@@ -1,5 +1,6 @@
 
-import { StyleSheet, TextInput, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, TextInput, TouchableOpacity, View } from "react-native";
 import { styles } from "./styles";
 
 export const Task = ({
@@ -9,7 +10,34 @@ export const Task = ({
 }) => {
   return (
     <View style={styles.listItem}>
-      <TextInput value={title} editable={false} />
+      <TouchableOpacity onPress={() => onArchiveTask(id)}>
+        {state !== "TASK_ARCHIVED" ? (
+          <MaterialIcons
+            name="check-box-outline-blank"
+            size={24}
+            color="#26c6da"
+          />
+        ) : (
+          <MaterialIcons name="check-box" size={24} color="#26c6da" />
+        )}
+      </TouchableOpacity>
+      <TextInput
+        placeholder="Input Title"
+        value={title}
+        editable={false}
+        style={
+          state === "TASK_ARCHIVED"
+            ? styles.listItemInputTaskArchived
+            : styles.listItemInputTask
+        }
+      />
+      <TouchableOpacity onPress={() => onPinTask(id)}>
+        <MaterialIcons
+          name="star"
+          size={24}
+          color={state !== "TASK_PINNED" ? "#eee" : "#26c6da"}
+        />
+      </TouchableOpacity>
     </View>
   );
 };
